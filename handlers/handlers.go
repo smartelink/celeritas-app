ac65737f840f9b3a9f1f537ca2c96b105005ade3
asgers <admin@smartelink.com>
2023-01-18 16:27:10 -0500

    Initial entry

---
+++ handlers/handlers.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"myapp/data"
+	"net/http"
+
+	"github.com/smartelink/celeritas"
+)
+
+// Handlers is the type for handlers, and gives access to Celeritas and models
+type Handlers struct {
+	App    *celeritas.Celeritas
+	Models data.Models
+}
+
+// Home is the handler to render the home page
+func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
+	err := h.render(w, r, "home", nil, nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering:", err)
+	}
+}
